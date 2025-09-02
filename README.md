57efbd29d74eb92eab6f9e19b5472753fb8b25a0
kimaad <kim.andre.dev@gmail.com>
2025-09-02 20:47:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GITHUB and Terraforming
+
+## Description will come after a while
+
+### Be cool, drink som tee
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type = string
+  default = "west europe"
+}
+
+variable "rg_name" {
+  type = string
+  default = "rg-gh-tf-practice-01"
+}
